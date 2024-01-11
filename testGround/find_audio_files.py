a0977e1217c9a9368a9ea32a51692c66402d8deb
jasonchess99 <jasonchess99@gmail.com>
2024-01-10 22:06:18 -0600

    please study and rewrite code in your own understanding.

--- testGround/find_audio_files.py
+++ testGround/find_audio_files.py
@@ -14,11 +14,11 @@ def list_audio_files(directory, extensions=('mp3', 'wav', 'ogg')):
 
 def save_to_text_file(file_list, output_file='audio_files.txt'):
     with open(output_file, 'w') as file:
-        # Write only the file names to the text file
-        file.write("\n".join(file_list))
+        # Write only the file names (without extensions) to the text file
+        file.write("\n".join(os.path.splitext(file)[0] for file in file_list))
 
 # Replace 'your_directory' with the path to your music directory
-music_directory = 'your_directory'
+music_directory = '/home/jason/Projects/mp3toSpotify/music_library/'
 audio_files_list = list_audio_files(music_directory)
 
 # Replace 'your_output_file.txt' with the desired output file name
